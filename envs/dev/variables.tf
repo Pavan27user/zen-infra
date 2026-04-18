c189581b7ff91941d50bceab5a8f4c339e584749
AR Shankar <arsr319@gmail.com>
2026-04-18 12:09:41 +0530

    updated additional scripts

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -11,6 +11,7 @@ variable "jwt_secret" {
 }
 
 variable "github_org" {
-  description = "GitHub username or organization that owns zen-pharma-frontend and zen-pharma-backend (e.g. john-smith)"
+  description = "GitHub username or organization that owns zen-pharma-frontend and zen-pharma-backend"
   type        = string
+  default     = "ravdy"
 }
